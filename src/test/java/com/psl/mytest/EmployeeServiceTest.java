7cb18a35eb9153610e6873e5ed63e377bde96b06
ketaki_yadwadkar <ketaki_yadwadkar@persistent.co.in>
2016-08-03 14:58:08 +0530

    Some more changes

--- src/test/java/com/psl/mytest/EmployeeServiceTest.java
+++ src/test/java/com/psl/mytest/EmployeeServiceTest.java
@@ -5,19 +5,22 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.internal.runners.JUnit4ClassRunner;
+//import org.junit.internal.runners.JUnit4ClassRunner;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.psl.Employee.entity.Employee;
 import com.psl.Employee.service.IEmployeeService;
 
 
-@RunWith(value=JUnit4ClassRunner.class)
-@ContextConfiguration(classes={EmployeeTestConfig.class})
+@RunWith(value=SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes={EmployeeTestConfig.class})
+@ContextConfiguration(locations = "EmployeeServiceTest-context.xml")
 public class EmployeeServiceTest {
 	
-	
+	@Autowired
 	private IEmployeeService service;
 	
 	
@@ -26,10 +29,6 @@ public class EmployeeServiceTest {
 //	public void setUp(){
 //		service = new EmployeeServiceStub();
 //	}
-	@Autowired
-	public void setService(IEmployeeService service) {
-		this.service = service;
-	}
 
 
 
@@ -37,6 +36,9 @@ public class EmployeeServiceTest {
 	public void getAllEmployeesTest(){
 		System.out.println("Here");
 		List<Employee> list = service.getAllEmployees();
+		for (Employee employee : list) {
+			System.out.println("~" + employee.getName());
+		}
 		System.out.println("size =  " + list.size());
 		Assert.assertEquals(2, list.size()); 
 		
